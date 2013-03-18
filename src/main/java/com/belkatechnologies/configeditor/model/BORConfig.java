0793b8f14149d6d9ab160b3fa7937c1e3e90cbf4
Nikita Khvorov <n.khvorov@gmail.com>
2013-03-18 14:58:32 +0300

    save and load buttons

---
+++ src/main/java/com/belkatechnologies/configeditor/model/BORConfig.java
@@ -0,0 +1,23 @@
+package com.belkatechnologies.configeditor.model;
+
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+import java.util.ArrayList;
+
+/**
+ * Author: Nikita Khvorov
+ * Date: 18.03.13
+ */
+
+@Root(name = "bor")
+public class BORConfig {
+    @ElementList(entry = "group")
+    private ArrayList<EmailGroup> mailing;
+    @ElementList
+    private ArrayList<Application> apps;
+
+    public ArrayList<Application> getApps() {
+        return apps;
+    }
+}
